fdb0cdbccb043d0b8217f8072bf0b07f46876a92
afekhaijoseph <46360244+afekhaijoseph@users.noreply.github.com>
2025-02-14 14:32:48 +0100

    update

--- src/pages/Notfoundpage/NotFoundPage.jsx
+++ src/pages/Notfoundpage/NotFoundPage.jsx
@@ -5,8 +5,8 @@ import styles from './notfoundpage.module.css'
 const NotFoundPage = () => {
   return (
     <div className = {styles.notfoundcontainer}>
-        <h1>How did you get here??</h1>
-        <Link className={styles.backhome}>Go Back!</Link>
+        <h1 className = {styles.notfoundtext}>How did you get here??</h1>
+        <Link className={styles.backhome} to = '/'>Go Back!</Link>
         <div className={styles.backhomecontainer}>
         
         </div>
